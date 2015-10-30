96f38df0c91ce87b08a94b711db43ab5b6c78f27
steveokay <mokaysteve@gmail.com>
2015-10-30 13:11:34 +0300

    Updates/changes

--- classes/demo.php
+++ classes/demo.php
@@ -1,30 +1,34 @@
 <?php
 
-require 'class.Address.inc';
-require 'class.Database.inc';
+/**
+ * Define autoloader.
+ * @param string $class_name
+ */
+function __autoload($class_name){
+    include 'class.' . $class_name . '.inc';
+}
 
-echo '<h2>Instantiating Address</h2>';
-$address = new Address();
+echo '<h2>Instantiating AddressResidence</h2>';
+$address_residence = new AddressResidence();
 
 echo '<h2> Setting Properties...</h2>';
-$address->city_name = 'Townsville';
-$address->street_address_1 = '555 Fake Street';
-$address->subdivision_name = 'State';
-$address->country_name = 'America';
-$address->address_type_id = 1;
-echo $address;
+$address_residence->city_name = 'Townsville';
+$address_residence->street_address_1 = '555 Fake Street';
+$address_residence->subdivision_name = 'State';
+$address_residence->country_name = 'America';
+$address_residence->address_type_id = 1;
+echo $address_residence;
+echo '<tt><pre>' . var_export($address_residence,TRUE) . '</pre></tt>'; 
 
 
 echo '<h2>Testing Address __construct with an array</h2>';
-$address_2 = new Address(array(
+$address_business = new AddressBusiness(array(
     'street_address_1' => '123 Phony Ave',
     'city_name' => 'Villageland',
     'subdivision_name' => 'Region',
     'country_name' => 'Canada',
 ));
-echo $address_2->display();
-
-echo '<h2>Address __toString</h2>';
-echo $address_2;
+echo $address_business;
+echo '<tt><pre>' . var_export($address_business,TRUE) . '</pre></tt>';  
 
 ?>
